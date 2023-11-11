23a31739f093f46db28b70dd858274818c5aa742
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-11-12 00:20:32 +0500

    styling done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,13 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <MainContent />
+      <MainContent
+        img="japan-pic.jpg"
+        country="Japan"
+        title="Mount Fuji"
+        date="1st Jan, 2021 -- 30th Jan, 2021"
+        summary="Japan is an island country in East Asia. It is in the northwest Pacific Ocean and is bordered on the west by the Sea of Japan, extending from the Sea of Okhotsk in the north toward the East China Sea, Philippine Sea, and Taiwan in the south"
+      />
     </div >
 
   )
--- my-app/src/MainContent.js
+++ my-app/src/MainContent.js
@@ -1,22 +1,24 @@
 import React from "react"
 
-export default function MainContent() {
+export default function MainContent(props) {
     return (
-      
-            <div className="container">
-                <div className="images">
-                    <img src="/japan-pic.jpg" alt="img-error" className="content-pic" />
-                </div>
+
+        <div className="container">
+            <div className="images">
+                <img src={`/${props.img}`} alt="img-error" className="content-pic" />
+            </div>
+            <div className="main">
                 <div className="content-details">
-                    <h4>Japan</h4>
+                    <h4>{props.country}</h4>
                     <span className="maps">View on Google Maps</span>
                 </div>
                 <div>
-                    <h1 className="content-summary">Mount Fuji</h1>
-                    <h4>1st Jan, 2021 -- 30th Jan, 2021</h4>
-                    <p className="main-text">Japan is an island country in East Asia. It is in the northwest Pacific Ocean and is bordered on the west by the Sea of Japan, extending from the Sea of Okhotsk in the north toward the East China Sea, Philippine Sea, and Taiwan in the south</p>
+                    <h1 className="content-summary">{props.title}</h1>
+                    <h4>{props.date}</h4>
+                    <p className="main-text">{props.summary}</p>
                 </div>
             </div>
-        
+        </div>
+
     )
 }
