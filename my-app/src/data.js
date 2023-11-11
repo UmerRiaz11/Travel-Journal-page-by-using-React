806f4bb91c046ea74a7149b7e29506a7f54338ef
Muhammad Umer Riaz <exclusiveumer@gmail.com>
2023-11-12 00:30:24 +0500

    pushed data in data.js file

---
+++ my-app/src/data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        img: "japan-pic.jpg",
+        country: "Japan",
+        title: "Mount Fuji",
+        date: "1st Jan, 2021 -- 30th Jan, 2021",
+        summary: "Japan is an island country in East Asia. It is in the northwest Pacific Ocean and is bordered on the west by the Sea of Japan, extending from the Sea of Okhotsk in the north toward the East China Sea, Philippine Sea, and Taiwan in the south",
+    },
+    {
+        img: "australia-pic.jpg",
+        country: "Australia",
+        title: "Bridge",
+        date: "1st Feb, 2021 -- 29th Feb, 2021",
+        summary: "Australia, officially the Commonwealth of Australia, is a sovereign country comprising the mainland of the Australian continent, the island of Tasmania, and numerous smaller islands. Australia is the largest country by area in Oceania and the world's sixth-largest country.",
+    },
+    {
+        img: "canada-pic.jpg",
+        country: "Canada",
+        title: "Hills & Lake",
+        date: "1st March, 2021 -- 30th March, 2021",
+        summary: "It is the second largest country in the world, but it has only one-half of one percent of the world's population. Canada features black-blue lakes, numerous rivers, majestic western mountains, rolling central plains, and forested eastern valleys.",
+    }
+]
